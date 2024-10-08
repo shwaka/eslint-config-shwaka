557dee4f683f36daf4cbbdc19dc02848948f19d6
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-10-08 15:48:12 +0900

    almost migrated to flat config

--- index.js
+++ index.js
@@ -2,14 +2,80 @@
 import js from '@eslint/js'
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import unusedImports from 'eslint-plugin-unused-imports'
+import importPlugin from 'eslint-plugin-import'
+import reactPlugin from 'eslint-plugin-react'
 
 export default tseslint.config(
   js.configs.recommended,
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname, // See note in https://typescript-eslint.io/getting-started/typed-linting/
+      },
+    },
+  },
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      "import": importPlugin,
+      "react": reactPlugin,
+    },
     rules: {
-      //　ここにルールを追加していきます
+      "indent": [
+        "error",
+        2,
+        {"SwitchCase": 1},
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "double",
+        { "avoidEscape": true }
+      ],
+      "semi": "error", // previously used typescript-eslint
+      "@typescript-eslint/explicit-function-return-type": [
+        2,
+        { "allowExpressions": true }
+      ],
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-unused-vars": "off", // provided by "unused-imports"
+      // "@typescript-eslint/semi": ["error", "never"], // removed
+      // "@typescript-eslint/member-delimiter-style": [
+      //   "error",
+      //   {
+      //     "multiline": {
+      //       "delimiter": "none",
+      //     },
+      //     "singleline": {
+      //       "delimiter": "comma",
+      //       "requireLast": false,
+      //     }
+      //   }
+      // ],
+      "@typescript-eslint/strict-boolean-expressions": [
+        "error",
+        {
+          "allowString": false,
+          "allowNumber": false,
+        }
+      ],
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+      ],
+      "sort-imports": 0,
+      "import/order": [2, { "alphabetize": { "order": "asc" } }],
+      "import/named": 0, // Language not found in 'prism-react-renderer'
+      "import/no-unresolved": 0, // ちゃんと設定できてないせいか大量に出てきてしまう
+      "react/jsx-fragments": ["error", "element"], // <> ではなく <Fragment> を使う
     }
   }
 )
@@ -50,57 +116,6 @@ const config = {
     }
   },
   "rules": {
-    "indent": [
-      "error",
-      2,
-      {"SwitchCase": 1},
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double",
-      { "avoidEscape": true }
-    ],
-    "semi": "off", // use typescript-eslint
-    "@typescript-eslint/explicit-function-return-type": [
-      2,
-      { "allowExpressions": true }
-    ],
-    "@typescript-eslint/no-inferrable-types": "off",
-    "@typescript-eslint/no-unused-vars": "off", // provided by "unused-imports"
-    "@typescript-eslint/semi": ["error", "never"],
-    "@typescript-eslint/member-delimiter-style": [
-      "error",
-      {
-        "multiline": {
-          "delimiter": "none",
-        },
-        "singleline": {
-          "delimiter": "comma",
-          "requireLast": false,
-        }
-      }
-    ],
-    "@typescript-eslint/strict-boolean-expressions": [
-      "error",
-      {
-        "allowString": false,
-        "allowNumber": false,
-      }
-    ],
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
-    ],
-    "sort-imports": 0,
-    "import/order": [2, { "alphabetize": { "order": "asc" } }],
-    "import/named": 0, // Language not found in 'prism-react-renderer'
-    "import/no-unresolved": 0, // ちゃんと設定できてないせいか大量に出てきてしまう
-    "react/jsx-fragments": ["error", "element"], // <> ではなく <Fragment> を使う
   }
 }
 
