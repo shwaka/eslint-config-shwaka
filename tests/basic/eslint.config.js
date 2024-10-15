a75ccb5a93abefc01644b81d1986c40651720275
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-10-15 13:35:40 +0900

    add tests/basic/

---
+++ tests/basic/eslint.config.js
@@ -0,0 +1,9 @@
+import tseslint from "typescript-eslint"
+import configShwaka from "eslint-config-shwaka"
+
+export default tseslint.config(
+  {
+    extends: [...configShwaka],
+    files: ['**/*.{ts,tsx}']
+  },
+)
---
+++ tests/basic/test.ts
@@ -0,0 +1,3 @@
+function foo() {
+  const s = 'foo'
+}
