1c356663529dc0cb1758df49229b1ac6a6885485
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-10-15 14:07:39 +0900

    add tests

--- tests/basic/test.ts
+++ tests/basic/test.ts
@@ -7,3 +7,23 @@ var variableDeclaredWithVar = 3
 function functionWithoutReturnType() {
   return
 }
+
+// eslint-disable-next-line quotes
+const stringWithSingleQuotes = 'foo'
+
+function wrongIndent(): void {
+  // eslint-disable-next-line indent
+    return
+}
+
+function falsyValue(): void {
+  const falsy = 0
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (falsy) {
+    return
+  }
+  return
+}
+
+// These are allowed in basic config
+const lineWithSemi = 5;
