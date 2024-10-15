45b67f2a78a60f6177b8270cec37121ccbb8a1a6
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-10-15 15:09:53 +0900

    add tests/react-vite/

---
+++ tests/react-vite/eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { eslintConfigShwakaReact } from 'eslint-config-shwaka'
+
+export default tseslint.config(
+  ...eslintConfigShwakaReact,
+  { ignores: ['dist'] },
+  {
+    // ↓これがあると Cannot redefine plugin "@typescript-eslint" って出る
+    // extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      // ↓これがあると Cannot redefine plugin "react-hooks" って出る
+      // 'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
