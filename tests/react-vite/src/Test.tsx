bf7a7b51d182dc0478158fff08732d1cf7cac063
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-10-15 16:55:19 +0900

    downgrade eslint from 9.11.1 to 8.57.1 to avoid "TypeError: context.getSource is not a function"

--- tests/react-vite/src/Test.tsx
+++ tests/react-vite/src/Test.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable unused-imports/no-unused-vars */
 
+import { useState } from "react"
 import { Fragment } from "react/jsx-runtime"
 
 // export was added to avoid react-refresh/only-export-components
@@ -18,5 +19,16 @@ function ExplicitFragmentAllowed(): JSX.Element {
       <div>foo</div>
       <div>bar</div>
     </Fragment>
+  )
+}
+
+function HookInIf(): JSX.Element {
+  // ↓ constant condition にならないように parseInt を使用した
+  if (parseInt("1", 10) < 2) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [count, setCount] = useState(0)
+  }
+  return (
+    <div>foo</div>
   )
 }
