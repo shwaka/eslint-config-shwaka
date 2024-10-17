8a06e468056249b2ddb627c76215686d4111b94c
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2024-10-17 17:41:12 +0900

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export const eslintConfigShwakaBasic: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
+export const eslintConfigShwakaStylistic: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
+export const eslintConfigShwakaReact: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
+export const eslintConfigShwaka: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
+export default eslintConfigShwaka;
