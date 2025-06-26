7f239e58629b3cbb0aea39500ac20cc5eafe3374
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-26 16:33:53 +0900

    add run-tests.sh

---
+++ scripts/run-tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eu
+
+cd "$(dirname "$0")"
+# SCRIPTS_DIR=$(pwd)
+
+cd "$(git rev-parse --show-toplevel)" # go to the root of the repository
+ROOT_DIR=$(pwd)
+
+TEST_DIR="$ROOT_DIR/tests"
+TARGETS=$(ls "$TEST_DIR")
+
+for target in $TARGETS; do
+    echo
+    echo -e "\e[32m--- $target\e[m"
+    cd "$TEST_DIR/$target"
+    echo "npm ci > /dev/null"
+    npm ci > /dev/null
+    echo "npm run eslint"
+    npm run eslint
+done
