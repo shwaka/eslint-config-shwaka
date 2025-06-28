c073c89ea2953d2df9d0a4a741edc66837d93f63
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-28 11:37:57 +0900

    add test for jsx-tag-spacing

--- tests/react-vite/src/StylisticTest.tsx
+++ tests/react-vite/src/StylisticTest.tsx
@@ -17,7 +17,7 @@ export function IndentInConditionalJSX({ isFoo }: { isFoo: boolean }): JSX.Eleme
   )
 }
 
-function MaxPropsPerLine(): JSX.Element {
+function JsxMaxPropsPerLine(): JSX.Element {
   // Writing multiple props in the same line should be allowed
   return (
     <div
@@ -27,4 +27,15 @@ function MaxPropsPerLine(): JSX.Element {
       test
     </div>
   )
+}
+
+function JsxTagSpacing(): JSX.Element {
+  const foo = <div />
+  // eslint-disable-next-line @stylistic/jsx-tag-spacing
+  const bar = <div/>
+  const baz = (
+    // eslint-disable-next-line @stylistic/jsx-tag-spacing
+    <div className="my-div"/>
+  )
+  return (<div>foo</div>)
 }
