4857f714ee434c1f009525053dcac54bb7f8c127
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-06-29 16:56:52 +0900

    fix tsconfig.json; implementing index.test.ts

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,13 @@
 import { describe, it, expect } from "vitest"
+import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint"
 import { eslintConfigShwakaReact } from "./index"
 
+function getNames(configArray: FlatConfig.ConfigArray): string[] {
+  return configArray.map((config) => config.name)
+}
+
 describe("eslintConfigShwakaReact", () => {
-  it("should be an object", () => {
-    expect(typeof eslintConfigShwakaReact).toBe("object")
+  it("should contain shwaka/react", () => {
+    expect(getNames(eslintConfigShwakaReact)).toContain("shwaka/react")
   })
 })
