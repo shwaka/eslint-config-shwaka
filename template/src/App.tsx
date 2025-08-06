148934844efb9cba6d77060d09cfc56637c59bd7
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-08-06 14:53:37 +0900

    eslint in template/

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, type ReactElement } from "react"
 
-function App() {
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+
+function App(): ReactElement {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <React.Fragment>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </React.Fragment>
   )
 }
 
