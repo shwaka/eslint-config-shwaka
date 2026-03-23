1cf988fcdf8f711d9f3074c83d6ddd3eef715876
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-03-23 19:14:00 +0900

    fix: add local variable to fix type

--- no-numeric-rule-level.js
+++ no-numeric-rule-level.js
@@ -54,12 +54,14 @@ export const noNumericRuleLevel = {
             typeof value.value === "number" &&
             (value.value === 0 || value.value === 1 || value.value === 2)
           ) {
+            const severity = value.value
+
             context.report({
               node: value,
               messageId: "unexpectedNumeric",
-              data: { value: String(value.value) },
+              data: { value: String(severity) },
               fix(fixer) {
-                return fixer.replaceText(value, toSeverity(value.value))
+                return fixer.replaceText(value, toSeverity(severity))
               },
             })
             continue
@@ -69,16 +71,19 @@ export const noNumericRuleLevel = {
             const first = value.elements[0]
             if (
               first !== null &&
+              first !== undefined &&
               first.type === "Literal" &&
               typeof first.value === "number" &&
               (first.value === 0 || first.value === 1 || first.value === 2)
             ) {
+              const severity = first.value
+
               context.report({
                 node: first,
                 messageId: "unexpectedNumeric",
-                data: { value: String(first.value) },
+                data: { value: String(severity) },
                 fix(fixer) {
-                  return fixer.replaceText(first, toSeverity(first.value))
+                  return fixer.replaceText(first, toSeverity(severity))
                 },
               })
             }
