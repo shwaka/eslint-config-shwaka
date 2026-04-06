c12aefcc80505b638b345af9b97099b7daa5c18f
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-04-06 12:11:11 +0900

    chore: update react-hooks-plugin to 7.0.1

--- src/react.ts
+++ src/react.ts
@@ -11,19 +11,19 @@ export const eslintConfigShwakaReact = defineConfig(
   ...eslintConfigShwakaBasic,
   ...eslintConfigShwakaStylistic,
   ...eslintConfigShwakaImportOrder,
+  reactHooksPlugin.configs.flat.recommended,
   {
     name: "shwaka/react",
     plugins: {
       // @ts-expect-error 何故かエラーが出る
       react: reactPlugin,
-      "react-hooks": reactHooksPlugin,
+      // "react-hooks": reactHooksPlugin, // v5.2.0→v7.0.1で動作しなくなった
       "react-you-might-not-need-an-effect": reactYouMightNotNeedAnEffect,
     },
     rules: {
       "react/jsx-fragments": ["error", "element"], // <> ではなく <Fragment> を使う
       "react/jsx-no-leaked-render": "off", // @typescript-eslint/strict-boolean-expressions があれば十分
       "react/jsx-key": "error",
-      ...reactHooksPlugin.configs.recommended.rules,
       ...reactYouMightNotNeedAnEffect.configs.recommended.rules,
     },
     settings: {
